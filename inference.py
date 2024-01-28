0bc917bd6c6e838fdb12378b589838af24990e21
Niels <niels.rogge1@gmail.com>
2024-01-28 19:19:24 +0100

    Update script

--- inference.py
+++ inference.py
@@ -2,8 +2,11 @@ from PIL import Image
 import torch
 import requests
 
+# fetch new version of MiDaS
+torch.hub.help("intel-isl/MiDaS", "DPT_BEiT_L_384", force_reload=True)
+
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = torch.hub.load('NielsRogge/ZoeDepth:understanding_zoedepth', "ZoeD_N", pretrained=True).to(DEVICE).eval()
+model = torch.hub.load('NielsRogge/ZoeDepth:understanding_zoedepth', "ZoeD_N", pretrained=True, force_reload=True).to(DEVICE).eval()
 
 url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
 image = Image.open(requests.get(url, stream=True).raw)
